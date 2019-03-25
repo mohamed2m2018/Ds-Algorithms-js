a65c6f3069e02021079c531673e33211db7e6ac4
unknown <mohamed2m2018@gmail.com>
2019-03-25 17:04:29 +0200

    Adding reverseInt solution using toString

---
+++ reverseInt.js
@@ -0,0 +1,37 @@
+//Solution using toString
+const reverseInt = (number) => {
+  let negative = false;
+
+  //If last number is zero git rid of it
+  while (number % 10 === 0) {
+    number = number / 10;
+  }
+
+  //if number is negative, turn it to positive
+  if (number < 0) {
+    negative = true;
+    number = Math.abs(number);
+  }
+
+  //turn the string into array then reverse then join it after checking whether 
+
+  //it's positive or negative
+
+  negative
+    ? console.log(
+        number
+          .toString()
+          .split('')
+          .reverse('')
+          .join('') * -1
+      )
+    : console.log(
+        number
+          .toString()
+          .split('')
+          .reverse('')
+          .join('')
+      );
+};
+
+reverseInt(-100);
