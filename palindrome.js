c565cb8dcb721262a50d733d90ab0f337b5c6251
unknown <mohamed2m2018@gmail.com>
2019-03-25 16:30:22 +0200

    Adding solution using every helper function

--- palindrome.js
+++ palindrome.js
@@ -1,19 +1,28 @@
-const palindrome=(str)=>{
+const palindrome = (str) => {
+  let reversed = '';
 
-    let reversed='';
+  for (element of str) {
+    reversed = element + reversed;
+  }
 
-    for (element of str){
-
-        reversed=element+reversed;
-    }
-
-    if(reversed===str)
-        console.log('this word is palindrome')
-    else
-        console.log('this world is not palindrome')    
-}
-
+  if (reversed === str) console.log('this word is palindrome');
+  else console.log('this world is not palindrome');
+};
 
 palindrome('abba');
 palindrome('salah');
-palindrome('a7a')
+
+//Solution2 using every helper
+
+const palindrome2 = (str) => {
+  palindromed = str.split('').every((char, index) => {
+    return char === str[str.length - 1 - index];
+  });
+
+  palindromed
+    ? console.log('this word is palindrome')
+    : console.log('this word is not palindrome');
+};
+
+palindrome2('abba');
+palindrome2('salah');
